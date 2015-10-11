614869f200b9c09452893f1898c62ace963844a4
Kairi Papstel <kpapstel@klass2-17-l.intra.itcollege.ee>
2015-10-11 11:05:49 +0300

    praktika 2

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,8 @@
 package teema1;
 
+
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -17,5 +20,54 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application
+{
+
+    public void start(Stage primaryStage) throws Exception
+    {
+        Foor foor = new Foor();
+
+
+        for (int i = 0; i < 5; i++) {
+            performRed(foor);
+            performYellow(foor, "down");
+            performGreen(foor);
+            performYellow(foor, "up");
+        }
+    }
+
+    private void performRed(Foor foor)
+    {
+        foor.vahetaPunast();
+        foor.paus(3);
+    }
+
+    private void performYellow(Foor foor, String cicle)
+    {
+        if (cicle.equals("down")) {
+            foor.vahetaKollast();
+            foor.paus(0.5);
+            foor.vahetaKollast();
+            foor.vahetaPunast();
+        } else if (cicle.equals("up")) {
+        foor.vahetaKollast();
+        foor.paus(0.5);
+        foor.vahetaKollast();
+    }
+    }
+
+    private void performGreen(Foor foor)
+    {
+        foor.vahetaRohelist();
+        foor.paus(1.0);
+        foor.vahetaRohelist();
+
+        for (int i = 0; i < 6; i++)
+        {
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+        }
+
+        foor.vahetaRohelist();
+    }
 }
--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -1,8 +1,78 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lib.Foor;
+
 /**
  * Täpselt nagu oli fooriga, on nüüd ristmikuga. Kuidas esiteks
  * ristmik üles ehitada loe Foori README.md-st: https://github.com/KristerV/javaHarjutused
  */
-public class Peamurdja2_ristmik {
+public class Peamurdja2_ristmik extends Application{
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor1 = new Foor( Foor. YLEMINE, primaryStage);
+        Foor foor2 = new Foor( Foor. ALUMINE, primaryStage);
+        Foor foor3 = new Foor( Foor. PAREM, primaryStage);
+        Foor foor4 = new Foor( Foor. VASAK, primaryStage);
+
+        //foor1.vahetaRohelist();
+        //rohelisestPunaseks(foor1);
+
+        vertikaal(foor1, foor2);
+        horisontaal(foor3, foor4);
+
+
+    }
+
+    public void vertikaal(Foor foor1, Foor foor2){
+        foor1.vahetaRohelist();
+        foor2.vahetaRohelist();
+        rohelisestPunaseks(foor1);
+        rohelisestPunaseks(foor2);
+        punasestRoheliseks(foor1);
+        punasestRoheliseks(foor2);
+    }
+
+    public void rohelisestPunaseks(Foor foor){
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaRohelist();
+        foor.paus(0.5);
+        foor.vahetaKollast();
+        foor.paus(0.5);
+        foor.vahetaKollast();
+        foor.vahetaPunast();
+        foor.paus(2);
+        foor.vahetaPunast();
+    }
+
+    public void punasestRoheliseks (Foor foor){
+        foor.vahetaPunast();
+        foor.paus(0.5);
+        foor.vahetaKollast();
+        foor.paus(0.5);
+        foor.vahetaPunast();
+        foor.vahetaKollast();
+        foor.vahetaRohelist();
+    }
+
+    public void horisontaal(Foor foor3, Foor foor4) {
+        foor3.vahetaRohelist();
+        foor4.vahetaRohelist();
+        punasestRoheliseks(foor3);
+        punasestRoheliseks(foor4);
+        rohelisestPunaseks(foor3);
+        rohelisestPunaseks(foor4);
+
+
+    }
+
 }
